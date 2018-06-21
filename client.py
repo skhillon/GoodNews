0c4fd32b27e19bc13aa066f83dfcc1881d0e1359
Sarthak Khillon <skhillon@users.noreply.github.com>
2018-06-20 22:57:54 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,24 @@
-import requests
+import requests, json
 
-API_KEY = '88d3bf9f4bed4dfabda05fbfa5f3999e'
-BASE_URL = 'https://newsapi.org/v2/top-headlines'
+class Client:
+    __API_KEY = '88d3bf9f4bed4dfabda05fbfa5f3999e'
+    __BASE_URL = 'https://newsapi.org/v2/top-headlines'
+    __default_params = {'country': 'us', 'category': 'general'}
 
-def get_raw_json(**kwargs):
-    payload = kwargs
-    payload['apiKey'] = API_KEY
-    return requests.get(BASE_URL, params=payload).raise_for_status().json()
+    def __init__(self, test_mode=False):
+        self.test_mode = test_mode
+
+    def import_to_dict(self, filename=None, params=self.__default_params):
+        '''Imports data as dict. If test_mode, uses local file. Otherwise uses api'''
+        raw_json = None
+
+        if self.test_mode:
+            with open(filename) as f:
+                return json.load(f)       
+        else:
+            return self.__get_raw_json(params)
+
+    def __get_raw_json(self, **kwargs):
+        payload = kwargs
+        payload['apiKey'] = __API_KEY
+        return requests.get(__BASE_URL, params=payload).raise_for_status().json()
