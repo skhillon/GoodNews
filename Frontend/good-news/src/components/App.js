4b8b4bf4fe2bdf10a22347b8460f1e8758778d1e
Sarthak Khillon <sarthakkhillon24@gmail.com>
2018-07-04 12:49:05 -0700

    Using new material theme

---
+++ Frontend/good-news/src/components/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Button variant="raised" color="primary">
+        Click me
+      </Button>
+    );
+  }
+}
+
+// import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { Layout } from './Layout';
+
+// export class App extends React.Component {
+//   render() {
+//     return (
+//       <Router>
+//         <Layout />
+//       </Router>
+//     );
+//   }
+// };
+
+/*
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+*/
